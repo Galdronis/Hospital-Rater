c74aba9c77ff01959d3bd94e470263bd981fc062
Wolfgang <18wrightm@gmail.com>
2022-10-11 13:23:35 -0400

    Few changes to render the page

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -14,7 +14,7 @@ const Footer = () => {
           >
             ❤️
           </span>{' '}
-          by the Tech Thoughts team.
+          by the Receding Hairlines team.
         </h4>
       </div>
     </footer>
---
+++ client/src/components/homePage/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const homePage = () => {
+  
+
+  return (
+    <div>
+      <h1>HomePage Bish</h1>
+      
+          
+        
+    </div>
+  );
+};
+
+export default homePage;
