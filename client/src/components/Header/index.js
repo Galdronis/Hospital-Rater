a7e718a52ab823a9b67fb2e06f41d9ff78170df6
Nicholas148894 <nich.oettinger@gmail.com>
2022-10-11 14:25:31 -0400

    Various things related to the proper login page

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -5,7 +5,7 @@ const Header = () => {
     <header className="bg-primary text-light mb-4 py-3 flex-row align-center">
       <div className="container flex-row justify-space-between-lg justify-center align-center">
         <h1 className="m-0">HomePage</h1>
-        <p className="m-0">Get into the mind of a Hospital person</p>
+        <a className="m-0" href='../pages/Login.js'>Get into the mind of a Hospital person</a>
       </div>
     </header>
   );
