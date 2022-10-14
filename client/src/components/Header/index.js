42949553e92831516c6f5d4f6ab4fc17c572bf73
Wolfgang <18wrightm@gmail.com>
2022-10-14 08:55:54 -0400

    Finished a hamburger nave links. and added an additional page for reviews.

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,36 +1,16 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 const Header = () => {
+
   return (
     <header className="bg-primary text-light mb-4 py-3 flex-row align-center">
-      <div className="container flex-row justify-space-between-lg justify-center align-center">
-
+      <div className="container flex-row justify-center align-center">
 
         <h1 className="m-0">HomePage</h1>
-       
-        <div className='navBarLinks'>
-
-          <Link to="/">
-            <button className='navBarLinks1'>Homepage</button>
-          </Link>
-          <Link to="/login">
-            <button className='navBarLinks1'>Log In</button>
-          </Link>
-          <Link to="/signup">
-            <button className='navBarLinks1'>Sign Up</button>
-          </Link>
-          <Link to="/feed">
-            <button className='navBarLinks1'>View Hospitals</button>
-          </Link>
-        </div>
-       
-
+        
       </div>
     </header>
   );
 };
 
 export default Header;
-
-
---
+++ client/src/components/HospitalView/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const FirstHospital = () => {
+  return (
+    <div>
+       <h2>Children's Hospital</h2>
+      <h3>700 Children's Dr, Columbus, Ohio</h3>
+      <h4>Rating: 7 stars</h4>
+      <h5>Reviews</h5>
+      <div>This is a review.</div>
+
+    </div>
+  );
+};
+
+export default FirstHospital;
