95cef6609ded00d2b9fb721d3a39ed74648ae78f
michaelhill4 <106322946+michaelhill4@users.noreply.github.com>
2022-10-14 10:49:31 -0400

    login working front end

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,13 +1,17 @@
 import { gql } from '@apollo/client';
 
 export const LOGIN_USER = gql`
-mutation Mutation($username: String, $email: String, $password: String) {
-createUser(username: $username, email: $email, password: $password) {
- username
- email
- password
+mutation Login($email: String!, $password: String!) {
+  login(email: $email, password: $password) {
+    token
+    user {
+      _id
+      username
+      email
+    }
+  }
 }
-}
+
 `;
 export const ADD_PROFILE = gql`
 mutation Mutation($username: String, $email: String, $password: String) {
